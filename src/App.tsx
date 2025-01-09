adf80b63a9d34bdea55d8b30b1b6926cea1213fd
gobeli <etienne.gobeli@outlook.com>
2025-01-09 08:12:19 +0100

    use resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { createSignal, For } from 'solid-js'
+import { createResource, createSignal, For, Match, Show, Switch } from 'solid-js'
 import { LocationSearch } from './ui/location-search.tsx'
 import { Location } from 'ojp-sdk'
-import { effect } from 'solid-js/web';
-import { searchTrips, TripView } from './functions/ojp.ts';
+import { searchTrips } from './functions/ojp.ts';
 import { TripRow } from './ui/trip-row.tsx';
+import { Spinner } from './ui/icons/spinner.tsx';
 
 function App() {
   const [from, setFrom] = createSignal<Location>();
   const [to, setTo] = createSignal<Location>();
-  const [trips, setTrips] = createSignal<TripView[]>([]);
-  
-  effect(() => {
-    if (!from() || !to()) {
-      return;
+  const stations = () => ({ from: from(), to: to() });
+
+  const [trips] = createResource(stations, ({ from, to }) => {
+    if (!from || !to) {
+      return [];
     }
 
     const date = new Date();
     date.setHours(new Date().getHours() + 2);
 
-    searchTrips(from()!, to()!, date).then(setTrips)
+    return searchTrips(from!, to!, date);
   });
 
   return (
@@ -29,11 +29,22 @@ function App() {
           <LocationSearch label="Von" location={from()} onChange={setFrom} name="from" />
           <LocationSearch label="Nach" location={to()} onChange={setTo} name="to" />
         </div>
-
         <div>
-          <For each={trips()}>
-            {(item) => <TripRow trip={item} />}
-          </For>
+          <Switch>
+            <Match when={trips.state === "errored"}>
+              Ein Fehler ist aufgetreten
+            </Match>
+            <Match when={trips.state === "refreshing"}>
+              <div class="flex">
+                <Spinner />
+              </div>
+            </Match>
+            <Match when={trips.state === "ready"}>
+              <For each={trips()}>
+                {(item) => <TripRow trip={item} />}
+              </For>
+            </Match>
+          </Switch>
         </div>
       </form>
     </main>
